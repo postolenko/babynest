0f5739dca94c46681897023b9a7b4326f5611d88
postolenko <denis.postolenko@gmail.com>
2017-06-13 17:51:21 +0300

    Baby Nest

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # http://postolenko.github.io/babynest/index.html
-<!-- # http://postolenko.github.io/babynest/catalog.html -->
+# http://postolenko.github.io/babynest/catalog.html
 # http://postolenko.github.io/babynest/blog.html
 # http://postolenko.github.io/babynest/blog_one.html
 # http://postolenko.github.io/babynest/card.html
 # http://postolenko.github.io/babynest/bascket.html
+# http://postolenko.github.io/babynest/contacts.html
--- js/additional_scripts.js
+++ js/additional_scripts.js
@@ -1,196 +1,202 @@
 $(document).ready(function() {
 
-			(function($){
+	var ageRange;
+	var maxAge;
+	var nodes;
 
-				$(window).on("load",function(){
+	// ----------------------------------------------------
 
-					$(".scroll").mCustomScrollbar();
+	(function($){
 
-				});
+		$(window).on("load",function(){
 
-			})(jQuery);	
+			$(".scroll").mCustomScrollbar();
 
-			 var mySwiper = new Swiper('.swiper-container', {
-			    loop: true,
-			    speed: 1400,
-			    pagination: $(".swiper-pagination"),
-			    paginationClickable: true,
-			    effect: 'coverflow',
-		        grabCursor: true,
-		        centeredSlides: true,
-		        slidesPerView: 'auto',
-		        coverflow: {
-		            rotate: 0,
-		            stretch: 0,
-		            depth: 155,
-		            modifier: 1,
-		            slideShadows : true
-		        }
-			  });
+		});
 
-			 $(".feedback_slider_1").not('.slick-initialized').slick({
-				dots: true,
-				arrows: false,
-				speed: 800,
-				slidesToShow: 3,
-				slidesToScroll: 2,
-				centerMode: true,
-				autoplay: true,
-				autoplaySpeed: 7000,
-				 responsive: [
-				    {
-				      breakpoint: 1200,
-				      settings: {
-				        slidesToShow: 2,
-				        slidesToScroll: 1
-				      }
-				    },
-				    {
-				      breakpoint: 480,
-				      settings: {
-				        slidesToShow: 1,
-				        slidesToScroll: 1
-				      }
-				    }
-				  ]
-			});
+	})(jQuery);	
 
-			if($("#age_range_sl").length > 0) {
+	 var mySwiper = new Swiper('.swiper-container', {
+	    loop: true,
+	    speed: 1400,
+	    pagination: $(".swiper-pagination"),
+	    paginationClickable: true,
+	    effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflow: {
+            rotate: 0,
+            stretch: 0,
+            depth: 155,
+            modifier: 1,
+            slideShadows : true
+        }
+	  });
 
-				var ageRange = document.getElementById('age_range_sl');
-				var maxAge = 8;
-				var nodes;
+	 $(".feedback_slider_1").not('.slick-initialized').slick({
+		dots: true,
+		arrows: false,
+		speed: 800,
+		slidesToShow: 3,
+		slidesToScroll: 2,
+		centerMode: true,
+		autoplay: true,
+		autoplaySpeed: 7000,
+		 responsive: [
+		    {
+		      breakpoint: 1200,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		  ]
+	});
 
-				noUiSlider.create(ageRange, {
-					start: [1, 5],
-					connect: true,
-					behaviour: 'tap',
-					step: 1,
-					range: {
-						'min': 0,
-						'max': 6
-					},
-					pips: {
-						mode: 'steps',
-						stepped: true,
-						density: 1
-					}
-				});
+	if($("#age_range_sl").length > 0) {
 
-				nodes = [
-					document.getElementById('lower-value'),
-					document.getElementById('upper-value')
-				];
+		ageRange = document.getElementById('age_range_sl');
+		maxAge = 8;
+		// nodes;
 
-				ageRange.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
+		noUiSlider.create(ageRange, {
+			start: [1, 5],
+			connect: true,
+			behaviour: 'tap',
+			step: 1,
+			range: {
+				'min': 0,
+				'max': 6
+			},
+			pips: {
+				mode: 'steps',
+				stepped: true,
+				density: 1
+			}
+		});
 
-					nodes[handle].innerHTML = parseFloat( values[handle] );
+		nodes = [
+			document.getElementById('lower-value'),
+			document.getElementById('upper-value')
+		];
 
-					if( parseInt( $("#upper-value").text() ) > 4 && parseInt( $("#upper-value").text() ) <= 6 ) {
+		ageRange.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
 
-						$("#upper-value").text(maxAge);
+			nodes[handle].innerHTML = parseFloat( values[handle] );
 
-					}
+			if( parseInt( $("#upper-value").text() ) > 4 && parseInt( $("#upper-value").text() ) <= 6 ) {
 
-				});
+				$("#upper-value").text(maxAge);
 
 			}
 
-			$(".mosaik-thumbnails").isotope({
-			  itemSelector: ".thumbnail",
-			  percentPosition: true,
-			  masonry: {
-				  horizontalOrder: true,
-				  columnWidth: '.grid-sizer'
-				}
-			});
+		});
 
-			$(".blog-slider").not('.slick-initialized').slick({
-		      dots: true,
-		      arrows: false,
-		      speed: 700,
-		      slidesToShow: 1,
-		      fade: true,
-		      autoplay: true,
-		      autoplaySpeed: 17000,
-		      appendDots: $(".blog-slider-pagination"),
-		      zIndex: 1
-		    });
+	}
 
-			$(".cart-slider").not('.slick-initialized').slick({
-				dots: true,
-				arrows: false,
-				speed: 700,
-				slidesToShow: 1,
-				fade: true,
-				autoplay: true,
-				autoplaySpeed: 17000,
-				appendDots: $(".cart-slider-pagination"),
-				zIndex: 1
-			});
+	$(".mosaik-thumbnails").isotope({
+	  itemSelector: ".thumbnail",
+	  percentPosition: true,
+	  masonry: {
+		  horizontalOrder: true,
+		  columnWidth: '.grid-sizer'
+		}
+	});
 
-			$(".good-slider").not('.slick-initialized').slick({
-				dots: true,
-				arrows: false,
-				speed: 700,
-				slidesToShow: 1,
-				fade: true,
-				autoplay: true,
-				autoplaySpeed: 17000,
-				appendDots: $(".good-slider-pagination"),
-				zIndex: 1
-			});
+	$(".blog-slider").not('.slick-initialized').slick({
+      dots: true,
+      arrows: false,
+      speed: 700,
+      slidesToShow: 1,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 17000,
+      appendDots: $(".blog-slider-pagination"),
+      zIndex: 1
+    });
 
-			$(".card-promo-slider").not('.slick-initialized').slick({
-		      dots: true,
-		      arrows: false,
-		      speed: 700,
-		      slidesToShow: 1,
-		      fade: true,
-		      autoplay: true,
-		      autoplaySpeed: 17000,
-		      appendDots: $(".card-promo-pagination"),
-		      zIndex: 1
-		    });
+	$(".cart-slider").not('.slick-initialized').slick({
+		dots: true,
+		arrows: false,
+		speed: 700,
+		slidesToShow: 1,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 17000,
+		appendDots: $(".cart-slider-pagination"),
+		zIndex: 1
+	});
 
-			$("select").select2();
+	$(".good-slider").not('.slick-initialized').slick({
+		dots: true,
+		arrows: false,
+		speed: 700,
+		slidesToShow: 1,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 17000,
+		appendDots: $(".good-slider-pagination"),
+		zIndex: 1
+	});
 
-			$(".feedback_slider_2").not('.slick-initialized').slick({
-				dots: true,
-				arrows: false,
-				speed: 800,
-				slidesToShow: 3,
-				slidesToScroll: 2,
-				autoplay: true,
-				autoplaySpeed: 7000,
-				 responsive: [
-				    {
-				      breakpoint: 1200,
-				      settings: {
-				        slidesToShow: 2,
-				        slidesToScroll: 1
-				      }
-				    },
-				    {
-				      breakpoint: 480,
-				      settings: {
-				        slidesToShow: 1,
-				        slidesToScroll: 1
-				      }
-				    }
-				  ]
-			});
+	$(".card-promo-slider").not('.slick-initialized').slick({
+      dots: true,
+      arrows: false,
+      speed: 700,
+      slidesToShow: 1,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 17000,
+      appendDots: $(".card-promo-pagination"),
+      zIndex: 1
+    });
 
-			$(".photos-slider").not('.slick-initialized').slick({
-				dots: true,
-				arrows: false,
-				speed: 700,
-				slidesToShow: 1,
-				fade: true,
-				autoplay: true,
-				autoplaySpeed: 17000,
-		      	appendDots: $(".photos-slider-pagination")
-		    });
+	$("select").select2();
+
+	$(".feedback_slider_2").not('.slick-initialized').slick({
+		dots: true,
+		arrows: false,
+		speed: 800,
+		slidesToShow: 3,
+		slidesToScroll: 2,
+		autoplay: true,
+		autoplaySpeed: 7000,
+		 responsive: [
+		    {
+		      breakpoint: 1200,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		  ]
+	});
+
+	$(".photos-slider").not('.slick-initialized').slick({
+		dots: true,
+		arrows: false,
+		speed: 700,
+		slidesToShow: 1,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 17000,
+      	appendDots: $(".photos-slider-pagination")
+    });
 
 
 });
